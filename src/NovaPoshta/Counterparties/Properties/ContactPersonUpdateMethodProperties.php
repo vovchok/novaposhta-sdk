38aca110118311dd919e4fc945eeb3c59510fbea
Volodymyr V <wovchok@gmail.com>
2016-11-10 13:26:23 +0200

    added save, update contact person method support

---
+++ src/NovaPoshta/Counterparties/Properties/ContactPersonUpdateMethodProperties.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace NovaPoshta\Counterparties\Properties;
+
+class ContactPersonUpdateMethodProperties extends ContactPersonMethodProperties
+{
+    public function setRef($ref)
+    {
+        $this->__set('Ref', $ref);
+    }
+
+    public function getRef()
+    {
+        $this->__get('Ref');
+    }
+}
---
+++ src/NovaPoshta/Counterparties/Services/ContactPersonService.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace NovaPoshta\Counterparties\Services;
+
+use NovaPoshta\Configuration;
+use NovaPoshta\Services\Service;
+
+use NovaPoshta\Counterparties\Properties\ContactPersonMethodProperties;
+use NovaPoshta\Counterparties\Properties\ContactPersonUpdateMethodProperties;
+
+class ContactPersonService extends Service
+{
+    public function __construct(Configuration $config)
+    {
+        $this->modelName = 'ContactPerson';
+
+        parent::__construct($config);
+    }
+
+    public function save(ContactPersonMethodProperties $properties)
+    {
+        return $this->makeRequest(
+            'save',
+            \NovaPoshta\Counterparties\Responses\ContactPersonSaveResponse::class,
+            $properties
+        );
+    }
+
+    public function update(ContactPersonUpdateMethodProperties $properties)
+    {
+        return $this->makeRequest(
+            'update',
+            \NovaPoshta\Counterparties\Responses\ContactPersonSaveResponse::class,
+            $properties
+        );
+    }
+}
