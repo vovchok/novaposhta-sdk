651b042ae964971dc792a8698c6529edaeabe505
Volodymyr V <wovchok@gmail.com>
2016-11-17 00:33:13 +0200

    added array access support

--- src/NovaPoshta/Model.php
+++ src/NovaPoshta/Model.php
@@ -2,7 +2,7 @@
 
 namespace NovaPoshta;
 
-class Model {
+class Model implements \ArrayAccess {
 
     private $properties = [];
 
@@ -23,5 +23,25 @@ class Model {
     public function __set($key, $value)
     {
         $this->properties[$key] = $value;
+    }
+
+    public function offsetSet($key, $value)
+    {
+        $this->__set($key, $value);
+    }
+
+    public function offsetExists($key)
+    {
+        return isset($this->properties[$key]);
+    }
+
+    public function offsetUnset($key)
+    {
+        unset($this->properties[$key]);
+    }
+
+    public function offsetGet($key)
+    {
+        return $this->__get($key);
     }
 }
